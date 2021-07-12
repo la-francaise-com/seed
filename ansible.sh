0c4711b19464445712dada752917cb5b01a47a9c
Aurélien Maury <cto@wescale.fr>
2021-07-12 15:41:27 +0200

    comfy

--- ansible.sh
+++ ansible.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
 SEP="-----------------------------------------"
-LOG="seed/ansible:"
+LOG="* seed/ansible:"
 
-echo ${SEP} && echo "${LOG} Check if server already bootstrapped"
+echo ${SEP}
+echo "${LOG} Check if server already bootstrapped"
 if [ -d "/etc/ansible/facts.d" ] && [ "$(cat /etc/ansible/facts.d/seed.fact)" = "{\"planted\": true}" ]; then
     echo "${LOG} Server already bootstrapped... Stopping."
     exit 0
@@ -11,7 +12,8 @@ fi
 
 PROJECT_DIR=`realpath $(cd "$(dirname "${BASH_SOURCE[0]}")/../.." &> /dev/null && pwd)`
 
-echo ${SEP} && echo "${LOG} Checking prerequisites"
+echo ${SEP}
+echo "${LOG} Checking prerequisites"
 if [ -z "${PERSONAL_ACCESS_TOKEN}" ]; then
     echo "${LOG} Env var 'PERSONAL_ACCESS_TOKEN' should be defined."
     exit 2
@@ -57,33 +59,47 @@ echo "${LOG} Install direnv"
 wget https://github.com/direnv/direnv/releases/download/v2.28.0/direnv.linux-amd64 -O /usr/local/bin/direnv
 chmod 755 /usr/local/bin/direnv
 
-echo ${SEP} && echo "${LOG} Add Python3 backport install to default PATH"
+echo ${SEP}
+echo "${LOG} Add Python3 backport install to default PATH"
 grep PATH /etc/environment \
-|| echo 'PATH="/usr/bin:/usr/local/bin:/opt/rh/rh-python38/root/usr/bin:/opt/rh/rh-python38/root/usr/local/bin"' >> /etc/environment
+    || echo 'PATH="/usr/bin:/usr/local/bin:/opt/rh/rh-python38/root/usr/bin:/opt/rh/rh-python38/root/usr/local/bin"' >> /etc/environment
+[ ! -L /usr/bin/python3 ] && ln -s /opt/rh/rh-python38/root/usr/bin/python3 /usr/bin/python3
+[ ! -L /usr/bin/pip3 ] && ln -s /opt/rh/rh-python38/root/usr/bin/pip3 /usr/bin/pip3
 
-echo ${SEP} && echo "${LOG} Generate key for Github/host interactions"
-[ ! -d "/root/.ssh/id_github" ] && mkdir -p /root/.ssh && chmod 700 /root/.ssh
-[ ! -e "/root/.ssh/id_github" ] && ssh-keygen -t ed25519 -N "" -C "" -f /root/.ssh/id_github
-grep "Host github.com" /root/.ssh/config || \
-    echo "Host github.com" >> /root/.ssh/config && \
-    echo "  IdentityFile ~/.ssh/id_github" >> /root/.ssh/config
-
-echo ${SEP} && echo "${LOG} Please add this key to deploy keys at:"
-echo "   key ==> $(cat /root/.ssh/id_github.pub)"
-echo "   url ==> https://github.com/la-francaise-com/ansible-operation-center/settings/keys"
-echo 
-read -p "Then press Enter to continue" </dev/tty
-
+echo ${SEP}
 echo "${LOG} Disable selinux"
 sed -i -e 's/SELINUX=.*/SELINUX=disabled/g' /etc/sysconfig/selinux
 sed -i -e 's/SELINUX=.*/SELINUX=disabled/g' /etc/selinux/config
 
-echo ${SEP} && echo "${LOG} Clone ansible-operation-center repository"
+echo ${SEP}
+echo "${LOG} Generate key for Github/host interactions"
+[ ! -d "/root/.ssh/id_github" ] && mkdir -p /root/.ssh && chmod 700 /root/.ssh
+[ ! -e "/root/.ssh/id_github" ] && ssh-keygen -t ed25519 -N "" -C "" -f /root/.ssh/id_github
+grep "Host github.com" /root/.ssh/config &> /dev/null || \
+cat > /root/.ssh/config << EOF
+Host github.com
+  IdentityFile ~/.ssh/id_github
+EOF
+
+echo ${SEP}
+echo "  Register this key   :  $(cat /root/.ssh/id_github.pub)"
+echo "  as a deploy key here: https://github.com/la-francaise-com/ansible-operation-center/settings/keys"
+echo ${SEP}
+read -p "Then press <ENTER> to continue" </dev/tty
+
+echo ${SEP}
+echo "${LOG} Clone ansible-operation-center repository"
 git clone git@github.com:la-francaise-com/ansible-operation-center.git
 
-echo ${SEP} && echo "${LOG} Install pip requirements"
+echo ${SEP}
+echo "${LOG} Install pip requirements"
 pip3 install -U -r ansible-operation-center/requirements.txt
 
-echo ${SEP} && echo "${LOG} Mark server as bootstrapped"
+echo ${SEP}
+echo "${LOG} Mark server as bootstrapped"
 mkdir -p /etc/ansible/facts.d
 echo "{\"planted\": true}" > /etc/ansible/facts.d/seed.fact
+
+echo ${SEP}
+echo "${LOG} Server needs a reboot... now. (HINT: type 'reboot' and press <ENTER>)"
+echo ${SEP}
